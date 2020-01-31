bb003c3413866b38f342600388d98e7b1b6c977d
Mantvydas Luksas <mluksas@getvisibility.com>
2020-01-31 10:35:15 +0000

    Test a happy case for the 'last' function

--- src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
+++ src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
@@ -3,5 +3,17 @@ package com.gv.scala.learning.lists
 import org.scalatest.wordspec.AnyWordSpec
 
 class ListsTest extends AnyWordSpec {
+    "A list" when{
 
+      "empty" should{
+
+        "yield the last element as requested" in {
+
+          assertResult(5)(Lists.last(List(1, 2, 3, 4, 5)))
+
+        }
+
+      }
+
+    }
 }
