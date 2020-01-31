7992fbda66ab28735ad4b6e45f9d2a70583c5176
Mantvydas Luksas <mluksas@getvisibility.com>
2020-01-31 12:31:29 +0000

    Create test for single element list calculation

--- src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
+++ src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
@@ -3,35 +3,48 @@ package com.gv.scala.learning.lists
 import org.scalatest.wordspec.AnyWordSpec
 
 class ListsTest extends AnyWordSpec {
-    "A list" when{
-      "contains a single element" should{
-        "return its element as the last element" in{
+  "A list" when {
+    "contains a single element" should {
+      "return its element as the last element" in {
 
-          assertResult('b')(Lists.last('b' :: Nil))
-
-        }
-
-
-      }
-      "not empty" should{
-
-        "yield the last element as requested" in {
-
-          assertResult(5)(Lists.last(List(1, 2, 3, 4, 5)))
-          assertResult("b")(Lists.last(List("a", "b")))
-
-        }
+        assertResult('b')(Lists.last('b' :: Nil))
 
       }
 
-      "empty" should {
 
-        "throw a NoSuchElementException when the last element is requested" in {
-
-          assertThrows[NoSuchElementException](Lists.last(List()))
-
-        }
-
-      }
     }
+    "not empty" should {
+
+      "yield the last element as requested" in {
+
+        assertResult(5)(Lists.last(List(1, 2, 3, 4, 5)))
+        assertResult("b")(Lists.last(List("a", "b")))
+
+      }
+
+    }
+
+    "empty" should {
+
+      "throw a NoSuchElementException when the last element is requested" in {
+
+        assertThrows[NoSuchElementException](Lists.last(List()))
+
+      }
+
+    }
+  }
+
+  "A list" when {
+    "contains a single element" should {
+      "return a total of 1 element only" in {
+
+        assertResult(1)(Lists.noOfElements('b' :: Nil))
+
+      }
+
+
+    }
+
+  }
 }
