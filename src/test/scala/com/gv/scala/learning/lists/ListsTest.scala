256dbfd1a151ac76244b45ab3700538c62e06248
Mantvydas Luksas <mluksas@getvisibility.com>
2020-01-31 10:37:42 +0000

    Test last method with List[String]

--- src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
+++ src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
@@ -10,7 +10,7 @@ class ListsTest extends AnyWordSpec {
         "yield the last element as requested" in {
 
           assertResult(5)(Lists.last(List(1, 2, 3, 4, 5)))
-
+          assertResult("a")(Lists.last(List("a", "b")))
         }
 
       }
