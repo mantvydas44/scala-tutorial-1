875152bf9eb72e9aba3464f017127d52f45a0cfe
Mantvydas Luksas <mluksas@getvisibility.com>
2020-02-04 10:14:39 +0000

    Created flatten method with test cases

--- src/main/scala/com/gv/scala/learning/lists/Lists.scala
+++ src/main/scala/com/gv/scala/learning/lists/Lists.scala
@@ -1,6 +1,9 @@
 package com.gv.scala.learning.lists
 
+import java.util.NoSuchElementException
+
 import scala.annotation.tailrec
+
 
 object Lists {
 
@@ -19,8 +22,29 @@ object Lists {
   def noOfElements[Any](list: List[Any]) : Int = list match{
 
     case Nil => 0// If the list is empty NoSuchElementException is thrown
-
     case head :: tail => 1 + noOfElements(tail) // The list contains more than one element and so the method is called again recursively
   }
+
+  // Palindrome method
+  def palindrome[Int](list: List[Int]) : Boolean = list match {
+
+    case Nil => true
+
+    case head :: Nil => true
+
+    case list => (list.head == list.last && palindrome(list.tail.init))
+
+  }
+
+ def flatten[Any](list: List[Any]): List[Any] = list match{
+
+   case Nil => Nil
+   case (h:List[Any])::tail => flatten(h):::flatten(tail)
+   case h::tail => h::flatten(tail)
+
+    }
+
+
+
 
 }
--- src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
+++ src/test/scala/com/gv/scala/learning/lists/ListsTest.scala
@@ -57,4 +57,86 @@ class ListsTest extends AnyWordSpec {
 
     }
   }
+
+  "Palindrome list" when{
+
+    "contains nothing " should{
+
+      "be true if empty" in{
+
+        assertResult(true)(Lists.palindrome(Nil))
+      }
+
+
+    }
+
+    "contains a single element" should{
+
+      "return true" in {
+
+        assertResult(true)(Lists.palindrome(List(1)))
+
+
+      }
+
+    }
+
+    "contains many elements but the list is not palindrome" should{
+
+
+      "return false" in {
+
+        assertResult(false)(Lists.palindrome(List(1,2,3,4)))
+
+      }
+
+    }
+
+    "contains elements and is palindrome" should{
+
+      "return true" in{
+        assertResult(true)(Lists.palindrome(List(1,2,3,4,3,2,1)))
+        assertResult(false)(Lists.palindrome(List(1,2,3,4,3,7,1)))
+
+      }
+    }
+
+
+  }
+
+ "Flatten list" when{
+
+   "the list is flattened into two" should{
+
+     "return a result of a single list of any type" in {
+
+
+       assertResult(List(1,2,3,4,5,6))(Lists.flatten(List(List(1), 2, List(3, 4, 5, 6))))
+
+
+
+
+
+     }
+
+
+
+   }
+
+   "the list is empty" should{
+
+     "should return nil" in{
+
+       assertResult(Nil)(Lists.flatten(List()))
+
+
+
+     }
+
+   }
+
+ }
+
 }
+
+
